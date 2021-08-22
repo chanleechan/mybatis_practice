ab3242f6fc65a9a2d06617902fa064a98c805f3d
chan <kscks1234@kakao.com>
2021-08-23 00:08:16 +0900

    mabatis practice

---
+++ src/main/java/com/board/boardVo/BoardVo.java
@@ -0,0 +1,28 @@
+package com.board.boardVo;
+
+
+
+public class BoardVo {
+	private int board_num;
+	private String title;
+	private String contents;
+	
+	public int getBoard_num() {
+		return board_num;
+	}
+	public void setBoard_num(int board_num) {
+		this.board_num = board_num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+}
